5a7ad23c1cc4cf873cf394f1f8b23df1b44a631b
Mike MacDonald <crazymykl@gmail.com>
2025-04-09 12:01:51 -0400

    Add CDK Pipeline Skeleton

--- lib/scavenger-hunt-infrastructure-stack.ts
+++ lib/scavenger-hunt-infrastructure-stack.ts
@@ -1,16 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib"
+import { Construct } from "constructs"
+import { PipelineStack } from "./pipeline-stack"
 
-export class ScavengerHuntInfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class ScavengerHuntInfrastructureStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ScavengerHuntInfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new PipelineStack(this, "Pipeline")
   }
 }
