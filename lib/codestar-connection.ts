c6895777ea529f8a6841758858e044bb4886873c
Mike MacDonald <crazymykl@gmail.com>
2025-07-12 13:38:38 -0400

    Trigger CodePipeline on source push

--- lib/codestar-connection.ts
+++ lib/codestar-connection.ts
@@ -4,4 +4,7 @@ const connectionArn =
   "arn:aws:codeconnections:us-east-1:105227738989:connection/59a567b2-9acd-4080-a091-5d32be10d491"
 
 export const codestarConnection = (repo: string, branch: string) =>
-  CodePipelineSource.connection(repo, branch, { connectionArn })
+  CodePipelineSource.connection(repo, branch, {
+    connectionArn,
+    triggerOnPush: true,
+  })
